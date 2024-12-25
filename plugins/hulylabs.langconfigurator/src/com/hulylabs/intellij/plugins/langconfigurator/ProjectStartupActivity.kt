bd031983262da126be4f9016efcda5cd5b4f063c
Konstantin Zaitsev <konstantin.zaitsev@xored.com>
2024-12-25 18:20:55 +0700

    Update product icons and disable Ultimate advertiser

--- plugins/hulylabs.langconfigurator/src/com/hulylabs/intellij/plugins/langconfigurator/ProjectStartupActivity.kt
+++ plugins/hulylabs.langconfigurator/src/com/hulylabs/intellij/plugins/langconfigurator/ProjectStartupActivity.kt
@@ -2,6 +2,7 @@
 package com.hulylabs.intellij.plugins.langconfigurator
 
 import com.hulylabs.intellij.plugins.langconfigurator.gitignore.GitIgnoreSyncService
+import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
@@ -10,5 +11,6 @@ class ProjectStartupActivity() : ProjectActivity {
   override suspend fun execute(project: Project) {
     val service: GitIgnoreSyncService = project.service()
     service.syncState()
+    PropertiesComponent.getInstance().setValue("ide.try.ultimate.disabled", true)
   }
 }

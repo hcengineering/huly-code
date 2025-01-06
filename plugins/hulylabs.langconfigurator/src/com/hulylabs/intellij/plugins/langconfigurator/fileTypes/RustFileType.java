3940b7e00676a4fb7f0b5842a2e55b10a836be8a
Konstantin Zaitsev <konstantin.zaitsev@xored.com>
2025-01-06 11:20:35 +0700

    Add settings for "Format on save" action

--- plugins/hulylabs.langconfigurator/src/com/hulylabs/intellij/plugins/langconfigurator/fileTypes/RustFileType.java
+++ plugins/hulylabs.langconfigurator/src/com/hulylabs/intellij/plugins/langconfigurator/fileTypes/RustFileType.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright © 2024 Huly Labs. Use of this source code is governed by the Apache 2.0 license.
 package com.hulylabs.intellij.plugins.langconfigurator.fileTypes;
 
 import com.intellij.openapi.fileTypes.FileType;

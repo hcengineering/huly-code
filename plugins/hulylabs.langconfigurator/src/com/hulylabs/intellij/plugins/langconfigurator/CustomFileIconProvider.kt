b924bdb26c55efaf4bdb43d5930de798477c9ace
Konstantin Zaitsev <konstantin.zaitsev@xored.com>
2025-02-05 18:49:15 +0700

    Reverted platform icons, added a new default bundledColorScheme

--- plugins/hulylabs.langconfigurator/src/com/hulylabs/intellij/plugins/langconfigurator/CustomFileIconProvider.kt
+++ plugins/hulylabs.langconfigurator/src/com/hulylabs/intellij/plugins/langconfigurator/CustomFileIconProvider.kt
@@ -10,15 +10,11 @@ import javax.swing.Icon
 class CustomFileIconProvider : FileIconProvider {
 
   override fun getIcon(file: VirtualFile, flags: Int, project: Project?): Icon? {
-    if (file.fileType.name == "TOML") {
-      return getInstance().getIcon("icons/toml.svg", CustomFileIconProvider::class.java.classLoader)
-    }
     val ext = file.extension
     when (ext) {
       "rs" -> return getInstance().getIcon("icons/rust.svg", CustomFileIconProvider::class.java.classLoader)
       "ts", "jsx", "tsx" -> return getInstance().getIcon("icons/typescript.svg", CustomFileIconProvider::class.java.classLoader)
-      "md", "mdx" -> return getInstance().getIcon("icons/readme.svg", CustomFileIconProvider::class.java.classLoader)
-      "toml" -> return getInstance().getIcon("icons/toml.svg", CustomFileIconProvider::class.java.classLoader)
+      "zig" -> return getInstance().getIcon("icons/zig.svg", CustomFileIconProvider::class.java.classLoader)
       else -> return null
     }
   }
